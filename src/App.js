9c1e77486f8e572f6f4aa843897c9184ece5da6e
Swamy-ai <72182728+Swamy-ai@users.noreply.github.com>
2021-05-06 16:33:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import SearchGoogle from "./components";
+
+import "./App.css";
+
+function App() {
+  return <SearchGoogle />;
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,89 @@
+import { Component } from "react";
+
+import "./index.css";
+
+class SearchGoogle extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <h1 className="logo">GOOGLE</h1>
+        <input className="input" placeholder="Search google or type url" />
+        <div className="section">
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+          <div className="sub-section">
+            <p className="sub-logo">W</p>
+            <a href="www.google.co.in" className="text">
+              wikipedia
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default SearchGoogle;
